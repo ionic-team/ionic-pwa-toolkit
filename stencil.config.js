472a22383ec263f1520250f878ead0899b33c6d0
jgw96 <jgw9617@gmail.com>
2018-02-19 11:32:16 -0600

    chore(): update to latest

--- stencil.config.js
+++ stencil.config.js
@@ -1,12 +1,13 @@
+const sass = require('@stencil/sass');
+
 exports.config = {
-  collections: [
-    { name: '@stencil/router' },
-    { name: '@ionic/core' }
-  ],
   serviceWorker: {
     swSrc: 'src/sw.js'
   },
-  globalStyle: 'src/global/app.css'
+  globalStyle: 'src/global/app.css',
+  plugins: [
+    sass()
+  ]
 };
 
 exports.devServer = {
