bc0c5642403a9bc82a23930660ef4bda05314eff
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-08-17 02:13:52 +0200

    chore(): update to latest ionic

--- src/global/app.ts
+++ src/global/app.ts
@@ -1,10 +1,5 @@
-/*
-  This is temporarily commented out as the `setupConfig` method has been temporarily removed
-*/
-
 // import { setupConfig } from '@ionic/core';
 
-//setupConfig({
-  // uncomment the following line to force mode to be Material Design
-  // mode: 'md'
-//});
+// setupConfig({
+//   mode: 'ios'
+// });
