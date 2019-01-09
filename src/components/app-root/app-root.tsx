c1abb7cb042bb56a9035f61f3d5f4b0e530ac4ae
Simon Hänisch <simonhaenisch@users.noreply.github.com>
2019-01-09 21:32:03 +1300

    remove the custom service worker (#96)

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,33 +1,10 @@
-import { Component, Prop, Listen } from '@stencil/core';
+import { Component } from '@stencil/core';
 
 @Component({
   tag: 'app-root',
   styleUrl: 'app-root.css'
 })
 export class AppRoot {
-
-  @Prop({ connect: 'ion-toast-controller' }) toastCtrl: HTMLIonToastControllerElement;
-
-  /**
-   * Handle service worker updates correctly.
-   * This code will show a toast letting the
-   * user of the PWA know that there is a
-   * new version available. When they click the
-   * reload button it then reloads the page
-   * so that the new service worker can take over
-   * and serve the fresh content
-   */
-  @Listen('window:swUpdate')
-  async onSWUpdate() {
-    const toast = await this.toastCtrl.create({
-      message: 'New version available',
-      showCloseButton: true,
-      closeButtonText: 'Reload'
-    });
-    await toast.present();
-    await toast.onWillDismiss();
-    window.location.reload();
-  }
 
   render() {
     return (
