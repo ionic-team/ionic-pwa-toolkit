4011fa65df27494e4efa5db52aed2e7e4b083048
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-09-27 14:49:26 +0200

    chore(): update component.d.ts

--- src/components.d.ts
+++ src/components.d.ts
@@ -44,11 +44,11 @@ declare global {
 }
 
 declare namespace LocalJSX {
-  interface AppHome extends JSXBase.HTMLAttributes<HTMLAppHomeElement> {}
-  interface AppProfile extends JSXBase.HTMLAttributes<HTMLAppProfileElement> {
+  interface AppHome {}
+  interface AppProfile {
     'name'?: string;
   }
-  interface AppRoot extends JSXBase.HTMLAttributes<HTMLAppRootElement> {}
+  interface AppRoot {}
 
   interface IntrinsicElements {
     'app-home': AppHome;
@@ -62,7 +62,11 @@ export { LocalJSX as JSX };
 
 declare module "@stencil/core" {
   export namespace JSX {
-    interface IntrinsicElements extends LocalJSX.IntrinsicElements {}
+    interface IntrinsicElements {
+      'app-home': LocalJSX.AppHome & JSXBase.HTMLAttributes<HTMLAppHomeElement>;
+      'app-profile': LocalJSX.AppProfile & JSXBase.HTMLAttributes<HTMLAppProfileElement>;
+      'app-root': LocalJSX.AppRoot & JSXBase.HTMLAttributes<HTMLAppRootElement>;
+    }
   }
 }
 
